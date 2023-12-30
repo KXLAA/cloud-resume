0f04ed7d844adaf0b58aaa74070efea58d0bbab4
KXLAA <kolade.afode@yahoo.com>
2023-12-30 08:39:21 +0000

    feat: add viewer tracker code
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,65 @@
-import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
+function getTotalVisitorCount() {
+  return 100;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+function updateVisitorCount(count: string) {
+  const element = document.getElementById("visitor-count");
+
+  if (element) {
+    element.textContent = count;
+  }
+}
+
+// Function to generate a random unique identifier
+function generateUniqueID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+// Function to check if a cookie exists
+function getCookie(name: string): string {
+  const cookies = Object.fromEntries(
+    document.cookie.split("; ").map((v) => v.split(/=(.*)/s))
+  );
+
+  return cookies[name];
+}
+
+// Function to set a cookie
+function setCookie(name: string, value: string, days: number) {
+  let expires = "";
+
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+// Function to track unique visitors
+function trackUniqueVisitors() {
+  let uniqueID = getCookie("uniqueID");
+
+  if (!uniqueID) {
+    // If no unique identifier exists, generate one and set a cookie
+    uniqueID = generateUniqueID();
+    setCookie("uniqueID", uniqueID, 365); // Cookie expires in 365 days
+
+    // Increment your unique visitors count or send the uniqueID to your server for tracking
+    // Example: You could send an HTTP request to your server with the uniqueID
+    // axios.post('/track-visitor', { uniqueID });
+
+    console.log("New unique visitor tracked");
+  } else {
+    console.log("Returning visitor");
+  }
+
+  const totalVisitorCount = getTotalVisitorCount();
+  updateVisitorCount(Number(totalVisitorCount).toLocaleString());
+}
+
+trackUniqueVisitors();
